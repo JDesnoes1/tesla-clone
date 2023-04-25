13f33bb9949d7b835f93a9c06b902faa978b3337
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-04-25 10:04:54 +0200

    Redux For Cars

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from "styled-components"
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
 
 function Header() {
-  return (
+    const [burgerStatus, setBurgerStatus] = useState(false);
+    const cars = useSelector(selectCars)
+    return (
     <Container>
         <a>
             <img src="/images/logo.svg" alt="" />
         </a>
         <Menu>
-            <a href="#">Model S</a>
-            <a href="#">Model 3</a>
-            <a href="#">Model X</a>
-            <a href="#">Model Y</a>
+            {cars && cars.map((car, index)=>
+                <a key={index} href="#">{car}</a>
+            )}
+            
         </Menu>
         <RightMenu>
             <a href="#">Shop</a>
             <a href="#">Tesla Account</a>
-            <CustomMenu />
+            <CustomMenu onClick={() => setBurgerStatus(true)} />
         </RightMenu>
-        <BurgerNav>
+        <BurgerNav show={burgerStatus}>
+            <CloseWrapper>
+            <CustomClose onClick={() => setBurgerStatus(false)} />
+            </CloseWrapper>
+            {cars && cars.map((car, index)=>
+                <li key={index}><a href="#">{car}</a></li>
+            )}
             <li><a href="#">Existing Inventory</a></li>
             <li><a href="#">Used Inventory</a></li>
             <li><a href="#">Trade-in Inventory</a></li>
             <li><a href="#">Cybertruck</a></li>
             <li><a href="#">Roadster</a></li>
-            <li><a href="#">Existing Inventory</a></li>
-            <li><a href="#">Existing Inventory</a></li>
-            <li><a href="#">Existing Inventory</a></li>
-            <li><a href="#">Existing Inventory</a></li>
+    
         </BurgerNav>
     </Container>
   )
@@ -87,5 +95,29 @@ const BurgerNav = styled.div`
     right:0;
     background: white;
     width: 300px;
-    z-index: 100;
+    z-index: 16;
+    list-style: none;
+    padding: 20px;
+    display:flex;
+    flex-direction:column;
+    text-align : start;
+    transform: ${props => props.show ? "translateX(0)" : "translateX(100%)"};
+    transition : transform 0.2s;
+    li {
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    }
+    a{
+        font-weight: 600;
+    }
+    
+`
+
+const CustomClose = styled(CloseIcon)`
+    cursor: pointer;
+`
+
+const CloseWrapper = styled.div`
+    display:flex;
+    justify-content: flex-end;
 `
